59b5a925af31cf39a5fa3cf11b2080b41755a6e8
splintgate <128176415+splintgate@users.noreply.github.com>
2023-07-13 02:13:26 +0530

    homepg ctr, bg video, blog bdr radius, how v work

--- src/component/content/Methodology/index.js
+++ src/component/content/Methodology/index.js
@@ -44,8 +44,7 @@ function Methodology() {
                         <p className="content-text">
                             User experience (UX) and (UI) designs are crucial elements in creating a successful website or app. UX
                             focuses on understanding and improving the overall user experience, ensuring that the website or app is
-                            easy to use, intuitive, and enjoyable for the user. This involves analyzing user behavior, conducting
-                            user research, and creating wireframes and prototypes to test and improve the design
+                            easy to use, intuitive, and enjoyable for the user.
                         </p>
                     </div>
                     <div className="img" style={{ width: '100%' }}>
@@ -58,10 +57,7 @@ function Methodology() {
                         <img src="images/tool.png" alt="" className="icon" height="100" width="auto" /><br />
                         <span className="content-heading">Development</span><br /><br />
                         <p className="content-text">
-                            The development process requires technical expertise in programming languages and technologies, as well
-                            as design skills to ensure a user-friendly and visually appealing final product
-                            Development is splintgate's key role in providing digital solutions to our clients with utmost
-                            efficiency and productivity.
+                        The Development team at Splintgate plays a key role in delivering efficient and visually appealing digital solutions, utilizing technical expertise in programming languages, technologies, and design skills
                         </p>
                     </div>
                     <div className="img" style={{ width: '100%' }}>
@@ -75,48 +71,13 @@ function Methodology() {
                         <span className="content-heading">Launch</span><br /><br />
                         <p className="content-text">Determine what you want the website or app to achieve, and who your target
                             audience is
-                            Decide on the technology platform you want to use to build your website or app. Consider factors such as
-                            cost, functionality, and ease of use
-                            Once you are satisfied with the website/app, launch it to the public. This step may require a marketing
-                            plan to promote the launch and attract users
+                            ,decide on the technology platform you want to use to build your website or app.
                         </p>
                     </div>
                     <div className="img" style={{ width: '100%' }}>
                         <img src="images/5.png" height="250" width="auto" alt="" />
                     </div>
                 </div>
-
-                {/* <div className="step">
-                    <div className="content" style={{ width: '100%' }}>
-                        <img src="images/tool.png" alt="" className="icon" height="100" width="auto" /><br />
-                        <span className="content-heading">IT Consultancy</span><br />
-                        <p className="content-text">
-                            The consultancy service may involve an analysis of the organization's existing technology
-                            infrastructure, processes, and systems, as well as identifying areas for improvement and recommending
-                            solutions.
-                        </p>
-                    </div>
-                    <div className="img" style={{ width: '100%' }}>
-                        <img src="images/4.png" height="250" width="auto" alt="" />
-                    </div>
-                </div>
-
-                <div className="step">
-                    <div className="content" style={{ width: '100%' }}>
-                        <img src="images/tool.png" alt="" className="icon" height="100" width="auto" /><br />
-                        <span className="content-heading">Corporate Trainings </span><br />
-                        <p className="content-text">
-                            We help firms to acquire talent and train them to compete in the digitalized market to evlove and learn
-                            day to day to be successful in the industry. We at splintgate have expertise in-house trainers who can
-                            provide trainings on various technologies in the software development domain.
-                        </p>
-                    </div>
-                    <div className="img" style={{ width: '100%' }}>
-                        <img src="images/4.png" height="250" width="auto" alt="" />
-                    </div>
-                </div> */}
-
-
             </div>
         </>
     )
